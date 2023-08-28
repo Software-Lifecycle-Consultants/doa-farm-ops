8f96521afaca5f295d473f066e1eddb862a264f3
VishwaLakpriya <vishwalakpriya99@gmail.com>
2023-08-28 14:42:11 +0530

    Implemented farmer profile

---
+++ components/LandsTable.tsx
@@ -0,0 +1,176 @@
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Button from "@mui/material/Button";
+import { IconButton, Stack } from "@mui/material";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+interface Column {
+  id:
+    | "landname"
+    | "district"
+    | "division"
+    | "rent"
+    | "irrigation"
+    | "button"
+    | "icons";
+  label: string;
+  minWidth?: number;
+  align?: "right";
+  format?: (value: number) => string;
+}
+
+const columns: readonly Column[] = [
+  { id: "landname", label: "Land Name", minWidth: 170 },
+  { id: "district", label: "District", minWidth: 170 },
+  {
+    id: "division",
+    label: "DS Division",
+    minWidth: 170,
+  },
+  {
+    id: "rent",
+    label: "Land Rent",
+    minWidth: 170,
+  },
+  {
+    id: "irrigation",
+    label: "Mode of Irrigation",
+    minWidth: 170,
+  },
+  {
+    id: "button",
+    label: "",
+    minWidth: 170,
+  },
+  {
+    id: "icons",
+    label: "",
+    minWidth: 170,
+  },
+];
+
+interface Data {
+  landname: string;
+  district: string;
+  division: string;
+  rent: number;
+  irrigation: string;
+  button: string;
+  icons: string;
+}
+
+function createData(
+  landname: string,
+  district: string,
+  division: string,
+  rent: number,
+  irrigation: string,
+  button: string,
+  icons: string
+): Data {
+  return { landname, district, division, rent, irrigation, button, icons };
+}
+
+const rows = [
+  createData("Udumulla Land", "Colombo", "Kaduwela", 33, "Well", "", ""),
+  createData("Udumulla Land 2", "Colombo", "Kaduwela", 33, "Well", "", ""),
+  createData("Udumulla Land 3", "Colombo", "Kaduwela", 33, "Well", "", ""),
+];
+
+interface TableTitleProps {
+  title: string;
+}
+
+export default function LandsTable({ title }: TableTitleProps) {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <>
+                          {column.id === "button" ? (
+                            <TableCell key={column.id} align={column.align}>
+                              <Button>Add Crop</Button>
+                            </TableCell>
+                          ) : column.id === "icons" ? (
+                            <TableCell key={column.id} align={column.align}>
+                              <Stack direction="row" spacing={2}>
+                                <IconButton>
+                                  <EditNoteIcon />
+                                </IconButton>
+                                <IconButton>
+                                  <DeleteIcon />
+                                </IconButton>
+                              </Stack>
+                            </TableCell>
+                          ) : (
+                            <TableCell key={column.id} align={column.align}>
+                              {column.format && typeof value === "number"
+                                ? column.format(value)
+                                : value}
+                            </TableCell>
+                          )}
+                        </>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
