52958d101b52c290d235c4a93487d39811f4d4b1
VishwaLakpriya <vishwalakpriya99@gmail.com>
2023-10-20 17:47:11 +0530

    Implemented redux for updating crop functionality

--- redux/cropSlice.ts
+++ redux/cropSlice.ts
@@ -4,28 +4,34 @@ import { RootState } from './types';
 
 // Define the initial state of the 'crop' slice.
 const initialState: RootState["crop"] = [];
-
+// Create a Redux slice for managing crop data
 const cropSlice = createSlice({
     name: 'crop', // A unique name for this slice, used in the Redux store.
     initialState,  // The initial state of the 'crop' slice.
     reducers: {
-      
+      // Reducer to add a new crop to the state
       addCrop: (state, action) => {
-        state.push(action.payload);
+        const maxId = state.reduce((max, crop) => Math.max(max, Number(crop._id)), 0);
+        const newId = (maxId + 1).toString();
+        const newCrop = { ...action.payload, _id: newId };
+      state.push(newCrop);
       },
+      // Reducer to update an existing crop in the state
       updateCrop: (state, action) => {
-        const { landId, cropDetails } = action.payload;
-        const index = state.findIndex((crop) => crop.landId === landId);
+        const { landId,_id, cropDetails } = action.payload;
+        const index = state.findIndex((crop) => crop._id === _id);
         if (index !== -1) {
           state[index] = {
             landId,
+            _id,
             cropDetails: { ...state[index].cropDetails, ...cropDetails },
           };
         }
       },
+      // Reducer to delete a crop from the state
       deleteCrop: (state, action) => {
         const landIdToDelete = action.payload;
-        const indexToDelete = state.findIndex((crop) => crop.landId === landIdToDelete);
+        const indexToDelete = state.findIndex((crop) => crop._id === landIdToDelete);
         if (indexToDelete !== -1) {
           state.splice(indexToDelete, 1); // Remove the crop from the array.
         }
--- redux/types.ts
+++ redux/types.ts
@@ -11,7 +11,9 @@ interface Crop {
       loanObtained: number;
     };
     landId: string;
+    _id: string;
   }
+  // Define the structure of the Redux store's state using the RootState type.
   export type RootState = {
-    crop: Crop[];
+    crop: Crop[];// An array of Crop objects, representing the state of crop data.
   };
