91324200e53248ea2e0228ce974ad5d248a93ddc
iamarasekera <iamarasekera.dev@gmail.com>
2023-11-03 17:31:08 +0800

    Formatted all data files

--- data/farmerProfile.ts
+++ data/farmerProfile.ts
@@ -1,43 +1,43 @@
 export interface FarmerProfileData {
-    nic: string;
-    email: string;
-    firstname: string;
-    lastname: string;
-    address: string;
-    phonenumber: string;
-    household: string;
-    otherdetails: {
-        orgname: string,
-        orgaddress: string
-      },
-      landDetails: {
-        lname: string,
-        district: string,
-        dsdivison: string,
-        irrigationMode: string,
-      },
-      createdBy: string,
-      createdAt: string,
-  }
+  nic: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  address: string;
+  phonenumber: string;
+  household: string;
+  otherdetails: {
+    orgname: string,
+    orgaddress: string
+  },
+  landDetails: {
+    lname: string,
+    district: string,
+    dsdivison: string,
+    irrigationMode: string,
+  },
+  createdBy: string,
+  createdAt: string,
+}
 
 export const sampleFarmerProfileData: FarmerProfileData = {
-    nic: "99123456V",
-    email: "sugath@gmail.com",
-    firstname: "Sugath",
-    lastname: "Jayaweera",
-    address: "Pretty View Lane",
-    phonenumber: "+9471345678",
-    household: "3",
-    otherdetails: {
-      orgname: "Green Asia Pvt Ltd",
-      orgaddress: "115/A, Borella Road, Rajagiriya"
-    },
-    landDetails: {
-      lname: "Udumaulla blk1",
-      district: "Monaragala",
-      dsdivison: "Monaragal DV1",
-      irrigationMode: "Surface irrigation",
-    },
-    createdBy:"+9471345678",
-    createdAt:"YYYY-MM-DD hh:mm:ss",
-  };
+  nic: "99123456V",
+  email: "sugath@gmail.com",
+  firstname: "Sugath",
+  lastname: "Jayaweera",
+  address: "Pretty View Lane",
+  phonenumber: "+9471345678",
+  household: "3",
+  otherdetails: {
+    orgname: "Green Asia Pvt Ltd",
+    orgaddress: "115/A, Borella Road, Rajagiriya"
+  },
+  landDetails: {
+    lname: "Udumaulla blk1",
+    district: "Monaragala",
+    dsdivison: "Monaragal DV1",
+    irrigationMode: "Surface irrigation",
+  },
+  createdBy: "+9471345678",
+  createdAt: "YYYY-MM-DD hh:mm:ss",
+};
