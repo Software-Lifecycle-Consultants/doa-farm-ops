f5581043f5782fe04bf674de9f4ca1d6f0e0431f
iamarasekera <iamarasekera.dev@gmail.com>
2023-11-03 16:52:08 +0800

    Fixed Merge issue setting 'isCultivationLoan'.

--- app/add-crop/page.tsx
+++ app/add-crop/page.tsx
@@ -78,6 +78,7 @@ export default function AddCrop() {
   const handleCultivationLoanChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
+    setIsCultivationLoan(event.target.value);
     setFormData({
       ...formData,
       isCultivationLoan:event.target.value,
