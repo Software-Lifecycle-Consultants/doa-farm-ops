c241523602f672826f892a9b35bbb59d30823fd2
VishwaLakpriya <vishwalakpriya99@gmail.com>
2023-12-08 12:20:48 +0530

    Backend user implementation

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,6 +3,7 @@ const userController = require('../controllers/userController');
 
 const userRoute = express.Router();
 
-userRoute.post("/api/data/", userController.createUser);
+userRoute.post("/api/user/register/", userController.createUser);
+userRoute.post("/api/user/login/", userController.login);
 
 module.exports = userRoute;
