aa60d8fc09e2f1555851953d8f14ba570788a1ba
paranga <paranga@gmail.com>
2024-03-15 11:07:08 +0530

    Add a national emblem of Sri Lanka

--- app/page.tsx
+++ app/page.tsx
@@ -1,71 +1,94 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import SnackbarContent from "@mui/material/SnackbarContent";
 import Typography from "@mui/material/Typography";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Box from "@mui/material/Box";
 
 
 
 
 
 
-const action = (
-  <Button color="secondary" size="small" >
-    Read More
-  </Button>
-); 
 
 export default function RowAndColumnSpacing() {
   return (
-    <Grid container spacing={2} columns={16} marginTop={6} padding={16}>
-      <Grid xs={8}>
-        <Stack spacing={4} sx={{ maxWidth: 809 }} marginTop={8}>
-          <SnackbarContent
-            message="How DoA Crop Data platform helps you"
-            action={action}
-          />
-        </Stack>
-
-        <Typography
-          fontSize={48}
-          fontFamily="cursive"
-          fontStyle={"bold"}
-          gutterBottom
-          marginTop={4}
-          textAlign="left"
-        >
-          Update farmer crop costs to help us manage Agriculture sector
-          development better
-        </Typography>
-
-        <Typography
-          fontSize={20}
-          fontFamily="system-ui"
-          fontStyle={"italic"}
-          gutterBottom
-          marginTop={4}
-        >
-          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
-          aliqua.
-        </Typography>
-
-        <Button variant="contained" color="success">
-          Department of Agriculture Digitzation effort.
-        </Button>
-      </Grid>
-
-      {/* image goes here */}
-
-      <Grid xs={8} padding={8} >
+    <Grid>
+      <Box marginLeft={100} display="flex">
         <img
-          src="https://thumbs.dreamstime.com/z/hand-drawn-vector-illustration-golden-yellow-rice-ear-transparent-background-highly-detailed-realistic-stile-rice-oryza-104436718.jpg?ct=jpeg"
+          src="https://logowik.com/content/uploads/images/sri-lanka-government8434.logowik.com.webp"
           alt="random"
-          width="524"
-          height="500"
+          width="250"
+          height="175"
         />
+      </Box>
+      <Grid
+        container
+        spacing={2}
+        columns={16}
+        marginLeft={32}
+
+        // bgcolor={"black"}
+      >
+        <Grid xs={6}>
+          <Grid xs={16}>
+            <Box component="section" sx={{ border: "1px" }} marginTop={8}>
+              How DoA Crop Data platform helps you
+              <Button color={"secondary"} size="small">
+                Read More
+                <IconButton aria-label="arrow">
+                  <ArrowForwardIcon />
+                </IconButton>
+              </Button>
+            </Box>
+
+            <Typography
+              fontSize={48}
+              fontFamily="cursive"
+              fontStyle={"bold"}
+              gutterBottom
+              marginTop={2}
+              textAlign="left"
+            >
+              Update farmer crop costs to help us manage Agriculture sector
+              development better
+            </Typography>
+            <Typography
+              fontSize={20}
+              fontFamily="system-ui"
+              fontStyle={"italic"}
+              gutterBottom
+              marginTop={4}
+            >
+              The Department of Agriculture (DOA) functions under the Ministry
+              of Agriculture and is one of the largest government departments
+              with a high profile community of agricultural scientists and a
+              network of institutions covering different agro ecological regions
+              island wide
+            </Typography>
+            {/* Button class */}
+            <Grid marginTop={6}>
+              <Button variant="contained" color="success">
+                Department of Agriculture Digitzation effort.
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/* image goes here */}
+
+        <Grid xs={10} paddingLeft={8} paddingTop={8} marginTop={2}>
+          <img
+            src="https://casamiatours.com/wp-content/uploads/2021/03/Schermata-2021-03-19-alle-19.31.44-600x449.png"
+            alt="random"
+            width="550"
+            height="450"
+          />
+        </Grid>
+        <Box marginLeft={50} marginTop={4} component="section" sx={{ p: 4 }}>
+          <h1>Open source development by Software Consultants</h1>
+        </Box>
       </Grid>
     </Grid>
   );
