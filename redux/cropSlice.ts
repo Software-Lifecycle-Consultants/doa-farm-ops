def2afe2043c4a4e19621a4b9d1dcad67c8a628b
Chamika Nuwatha <86287034+ChamikaNuwantha@users.noreply.github.com>
2024-05-09 04:46:18 +0530

    Display Crop details retrieved from backend through redux store

--- redux/cropSlice.ts
+++ redux/cropSlice.ts
@@ -1,45 +1,58 @@
 // Import the createSlice function from Redux Toolkit.
-import { createSlice } from '@reduxjs/toolkit';
-import { RootState } from './types';
+import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { Crop, RootState } from './types';
+import { fetchUserData } from '@/api/fetchUserData';
+import axios from 'axios';
 
-interface Crop {
-  cropDetails: {
-    cropName: string;
-    season: string;
-    cropType: string;
-    totalSoldQty: string;
-    totalIncome: string;
-    reservedQtyHome: string;
-    reservedQtySeed: string;
-    noOfPicks: string;
-    loanObtained: number;
-  };
-  landId: string;
-  _id: string;
-}
 
 // Define the initial state of the 'crop' slice.
-const initialState: RootState["crop"] = [];
+const initialState: { crops: Crop[] | null } = {
+  crops: null,
+};
+
+export const fetchCrops = createAsyncThunk(
+  'user/fetchCrops',
+  async (userId: string) => {
+    // Fetch user data using the provided userId
+    const userData = await fetchUserData(userId);
+    // Return the user data fetched from the API
+    return userData.crops;
+  }
+);
+
+
 
 // Create a Redux slice for managing crop data
 const cropSlice = createSlice({
-  name: "crop",
+  name: "crops",
   initialState,
   reducers: {
     addCrop: (state, action) => {
-      state.push(action.payload);
+      state.crops?.push(action.payload);
     },
     updateCrop: (state, action) => {
       const { _id } = action.payload;
-      const index = state.findIndex((crop) => crop._id === _id);
+      const index = state.crops?.findIndex((crop) => crop._id === _id);
       if (index !== -1) {
         state[index] = { ...action.payload };
       }
     },
     deleteCrop: (state, action) => {
       const _idToDelete = action.payload;
-      state = state.filter((crop) => crop._id !== _idToDelete);
+      state.crops =
+        state.crops?.filter((crop) => crop._id !== _idToDelete) || null;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      // Handle successful fulfillment of fetchAndRegisterFarmer
+      .addCase(fetchCrops.fulfilled, (state, action: PayloadAction<Crop[]>) => {
+        state.crops = action.payload;
+      })
+      // Handle rejection of fetchAndRegisterFarmer
+      .addCase(fetchCrops.rejected, (state, action) => {
+        console.error("Error fetching crop details:", action.error);
+      })
   },
 });
   
@@ -47,9 +60,10 @@ const cropSlice = createSlice({
 export const { addCrop, updateCrop, deleteCrop  } = cropSlice.actions;
 
 // Define a selector function to extract the 'crop' state from the Redux store.
-export const selectAddCrop = (state: { crop: any; }) => state.crop;
-export const selectUpdateCrop = (state: { crop: any; }) => state.crop;
-export const selectDeleteCrop = (state: { crop: any; }) => state.crop;
+export const selectCrops = (state: RootState) => state.crop.crops;
+export const selectAddCrop = (state: RootState) => state.crop.crops;
+export const selectUpdateCrop = (state: RootState) => state.crop.crops;
+export const selectDeleteCrop = (state: RootState) => state.crop.crops;
 
 // Export the 'cropSlice.reducer' as the default export.
 export default cropSlice.reducer;
--- redux/types.ts
+++ redux/types.ts
@@ -7,35 +7,34 @@ export interface Auth {
     role: string;
     token: string;
   };
-  }
+}
 
-interface Crop {
-    cropDetails: {
-      cropName: string;
-      season: string;
-      cropType: string;
-      totalSoldQty: string;
-      totalIncome: string;
-      reservedQtyHome: string;
-      reservedQtySeed: string;
-      noOfPicks: string;
-      loanObtained: number;
-    };
-    landId: string;
-    _id: string;
-  }
-  export interface Land {
-    _id: string;
-    landName: string;
+export interface Crop {
+  cropName: string;
+  season: string;
+  cropType: string;
+  totalSoldQty: string;
+  totalIncome: string;
+  reservedQtyHome: string;
+  reservedQtySeed: string;
+  noOfPicks: string;
+  loanObtained: number;
+  userId: string;
+  landId: string;
+  _id: string;
+}
+export interface Land {
+  _id: string;
+  landName: string;
     district: string |null;
-    dsDivision: string;
-    landRent: string;
-    irrigationMode: string;
-    userId: string;
-    crops: any[]; // You may need to specify the type of this array if it's not always empty
-    // createdAt: string;
-    // updatedAt: string;
-    // __v: number;
+  dsDivision: string;
+  landRent: string;
+  irrigationMode: string;
+  userId: string;
+  crops: any[]; // You may need to specify the type of this array if it's not always empty
+  // createdAt: string;
+  // updatedAt: string;
+  // __v: number;
 }
 interface LandAndCrop {
   landId: string;
@@ -47,37 +46,39 @@ interface LandAndCrop {
 }
 
 export interface User {
-    _id: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    phoneNumber: string;
-    nic: string;
-    role: string;
-    address: string;
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  nic: string;
+  role: string;
+  address: string;
 }
 
 export interface FarmerDetails {
-    household: string;
-    orgName: string;
-    orgAddress: string;
+  household: string;
+  orgName: string;
+  orgAddress: string;
 }
 
 export interface OfficerDetails {
-    orgName: string;
-    orgAddress: string;
-    university: string;
+  orgName: string;
+  orgAddress: string;
+  university: string;
 }
 
-  // Define the structure of the Redux store's state using the RootState type.
-  export type RootState = {
-    auth: Auth;
-    crops: Crop[]; // An array of Crop objects, representing the state of crop data.
-    land: {
-      lands: Land[] | null;
+// Define the structure of the Redux store's state using the RootState type.
+export type RootState = {
+  auth: Auth;
+  crop: {
+    crops: Crop[] | null;
+  }; // An array of Crop objects, representing the state of crop data.
+  land: {
+    lands: Land[] | null;
     };// An array of Land objects, representing the state of land data.
-    landAndCrop: LandAndCrop;
-    user: { user: User | null };
-    farmer: { farmerDetails: FarmerDetails | null };
-    officer: { officerDetails: OfficerDetails | null };
-  };
+  landAndCrop: LandAndCrop;
+  user: { user: User | null };
+  farmer: { farmerDetails: FarmerDetails | null };
+  officer: { officerDetails: OfficerDetails | null };
+};
