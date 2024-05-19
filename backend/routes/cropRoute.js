85060f1665e0f85d3c971c75406f1dd9cff63c97
lasantha117 <weerasinghehwlp.20@uom.lk>
2024-05-19 09:18:33 +0530

    Delete crop feature backend

--- backend/routes/cropRoute.js
+++ backend/routes/cropRoute.js
@@ -5,5 +5,6 @@ const cropRoute = express.Router();
 
 cropRoute.post("/api/crop/add/", cropController.addCrop);
 cropRoute.get("/api/get/crops/:id", cropController.getCropByUserId);
+cropRoute.delete("/api/crop/delete/:id", cropController.deleteCrop);
 
 module.exports = cropRoute;
