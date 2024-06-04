6ab4887f8db29e275534de7e0da7e550f6399f25
lasantha117 <weerasinghehwlp.20@uom.lk>
2024-06-04 16:41:55 +0530

    added MUI material to zodErrrors

--- app/ZodErrors.tsx
+++ app/ZodErrors.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import Alert from '@mui/material/Alert';
 
 export function ZodErrors({ error }: { error: string[] }) {
-    if (!error || error.length === 0) return null;
-    return (
-        <>
-            {error.map((err: string, index: number) => (
-                <div key={index} style={{ color: 'red', fontSize: '12px', fontStyle: 'italic', marginTop: '4px', padding: '8px 0' }}>
-                    {err}
-                </div>
-            ))}
-        </>
-    );
+  if (!error || error.length === 0) return null;
+  return (
+    <>
+      {error.map((err: string, index: number) => (
+        <Alert key={index} severity="error">
+          {err}
+        </Alert>
+      ))}
+    </>
+  );
 }
