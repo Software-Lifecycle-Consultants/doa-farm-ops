9875a67acb58383708ca3daf05d6fc9170b8b786
iamarasekera <iamarasekera.dev@gmail.com>
2024-06-06 22:19:19 +0800

    Revert the changes made to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const app = express();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,18 +12,19 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Hardcoded MongoDB connection string
-const MONGODB_URL = 'mongodb://127.0.0.1:27017/doa';
+const URL = process.env.MONGODB_URL;
 
-mongoose.connect(MONGODB_URL, {
+mongoose.connect(URL, {
+    // useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    // useFindAndModify: false
 });
 
 const connection = mongoose.connection;
-connection.once("open", () => {
-    console.log("MongoDB Connection Success!");
-});
+connection.once("open", () =>{
+    console.log("MongoDB Connection Success!")
+})
 
 // Middleware to parse JSON body
 app.use(express.json());
@@ -32,12 +35,11 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-// Route imports
+//route imports
 const userRoute = require("./routes/userRoute");
 const cropRoute = require("./routes/cropRoute");
 const landRoute = require("./routes/landRoute");
-
-// Routes
+//routes
 app.use(userRoute);
 app.use(cropRoute);
 app.use(landRoute);
