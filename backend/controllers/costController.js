0381379230d80764f2b537d2d4626457e68df882
Chamika Nuwatha <86287034+ChamikaNuwantha@users.noreply.github.com>
2024-06-06 06:33:29 +0530

    developed codebase to save multiple recodes of a single cost type

--- backend/controllers/costController.js
+++ backend/controllers/costController.js
@@ -1,5 +1,5 @@
 const Labour = require("../models/labourCostModel");
-const Material = require("../models/materialCostCModel");
+const Material = require("../models/materialCostModel");
 const Machinery = require("../models/machineryCostModel");
 const Operation = require("../models/operationCostsModel");
 
@@ -17,6 +17,10 @@ const costController = {
       } = req.body;
 
       console.log("req.body", req.body);
+
+      let labourresponseData = {};
+      let materialresponseData = {};
+      let machineryresponseData = {};
 
       if (
         !labourCostDetails ||
@@ -60,127 +64,140 @@ const costController = {
       //   return res.status(400).json({ msg: "Invalid operation cost data" });
       // }
 
-      const { gender, isHired, qty, dailyWage, foodCost } = labourCostDetails;
+      ///////////////     Labour Cost    /////////////////////
 
-      if (!gender || !isHired || !qty || !dailyWage || !foodCost) {
-        return res.status(400).json({ msg: "Please fill in all fields." });
+      for (const labourdetails of labourCostDetails) {
+        const { gender, isHired, qty, dailyWage, foodCost } = labourdetails;
+
+        if (!gender || !isHired || !qty || !dailyWage || !foodCost) {
+          return res.status(400).json({ msg: "Please fill in all fields." });
+        }
       }
 
-      // Create new labour cost
-      const newLabourCost = new Labour({
-        cropId,
-        // operationCostId,
-        gender,
-        isHired,
-        qty,
-        dailyWage,
-        foodCost,
-      });
+      // Create new labour costs
+      await Labour.insertMany(
+        labourCostDetails.map((details) => ({
+          cropId,
+          ...details,
+        }))
+      )
+        .then((docs) => {
+          // docs contains the documents inserted with added _id fields
+          console.log("Labaour documents inserted to Collection");
 
-      // Save labour cost
-      const savedlabourCost = await newLabourCost.save();
+          // Create the labourCost array
+          const labourCost = docs.map((doc) => ({
+            _id: doc._id,
+            cropId: doc.cropId,
+            gender: doc.gender,
+            isHired: doc.isHired,
+            qty: doc.qty,
+            dailyWage: doc.dailyWage,
+            foodCost: doc.foodCost,
+          }));
 
-      // if (savedlabourCost) {
-      //   res.status(200).json({
-      //     _id: savedlabourCost.id,
-      //     cropId: savedlabourCost.cropId,
-      //     //operationCostId: savedlabourCost.id,
-      //     gender: savedlabourCost.gender,
-      //     isHired: savedlabourCost.isHired,
-      //     qty: savedlabourCost.qty,
-      //     dailyWage: savedlabourCost.dailyWage,
-      //     foodCost: savedlabourCost.foodCost
-      //   });
-      // } else {
-      //   return res.status(400).json({ msg: "Invalid labour cost data" });
-      // }
-
-      const { material, qtyUsed, materialCost } = materialCostDetails;
-
-      if (!material || !qtyUsed || !materialCost) {
-        return res.status(400).json({ msg: "Please fill in all fields." });
-      }
-
-      // Create new material cost
-      const newMaterialCost = new Material({
-        cropId,
-        // operationCostId,
-        material,
-        qtyUsed,
-        materialCost,
-      });
-
-      // Save material cost
-      const savedMaterialCost = await newMaterialCost.save();
-
-      // if (savedMaterialCost) {
-      //   res.status(200).json({
-      //     _id: savedMaterialCost.id,
-      //     cropId: savedMaterialCost.cropId,
-      //     // operationCostId: savedMaterialCost.id,
-      //     material: savedMaterialCost.material,
-      //     qtyUsed: savedMaterialCost.qtyUsed,
-      //     materialCost: savedMaterialCost.materialCost,
-      //   });
-      // } else {
-      //   return res.status(400).json({ msg: "Invalid material cost data" });
-      // }
-
-      const { method, isOwned, noUsed, days, machineryCost } =
-        machineryCostDetails;
-
-      if (!method || !isOwned || !noUsed || !days || !machineryCost) {
-        return res.status(400).json({ msg: "Please fill in all fields." });
-      }
-
-      // Create new machinery cost
-      const newMachineryCost = new Machinery({
-        cropId,
-        // operationCostId,
-        method,
-        isOwned,
-        noUsed,
-        days,
-        machineryCost,
-      });
-
-      // Save machinery cost
-      const savedMachineryCost = await newMachineryCost.save();
-
-      if (savedMachineryCost) {
-        res.status(200).json({
-          machineryCost: {
-            _id: savedMachineryCost.id,
-            cropId: savedMachineryCost.cropId,
-            // operationCostId: savedMachineryCost.operationCostId,
-            method: savedMachineryCost.method,
-            isOwned: savedMachineryCost.isOwned,
-            noUsed: savedMachineryCost.noUsed,
-            days: savedMachineryCost.days,
-            machineryCost: savedMachineryCost.machineryCost,
-          },
-          materialCost: {
-            _id: savedMaterialCost.id,
-            cropId: savedMaterialCost.cropId,
-            // operationCostId: savedMaterialCost.operationCostId,
-            material: savedMaterialCost.material,
-            qtyUsed: savedMaterialCost.qtyUsed,
-            materialCost: savedMaterialCost.materialCost,
-          },
-          labourCost: {
-            _id: savedlabourCost.id,
-            cropId: savedlabourCost.cropId,
-            // operationCostId: savedlabourCost.operationCostId,
-            gender: savedlabourCost.gender,
-            isHired: savedlabourCost.isHired,
-            qty: savedlabourCost.qty,
-            dailyWage: savedlabourCost.dailyWage,
-            foodCost: savedlabourCost.foodCost,
-          },
+          // Create the response data
+          labourresponseData = labourCost;
+        })
+        .catch((err) => {
+          console.error(err);
+          res
+            .status(500)
+            .json({ msg: "Error saving labour cost data to the database." });
         });
-      } else {
-        return res.status(400).json({ msg: "Invalid machinery cost data" });
+
+      ///////////////   Material Cost   /////////////////////
+
+      for (const materialdeails of materialCostDetails) {
+        const { material, qtyUsed, materialCost } = materialdeails;
+
+        if (!material || !qtyUsed || !materialCost) {
+          return res.status(400).json({ msg: "Please fill in all fields." });
+        }
       }
+
+      // Create new material costs
+      await Material.insertMany(
+        materialCostDetails.map((details) => ({
+          cropId,
+          ...details,
+        }))
+      )
+        .then((docs) => {
+          // docs contains the documents inserted with added _id fields
+          console.log("Material documents inserted to Collection");
+
+          // Create the labourCost array
+          const materialCost = docs.map((doc) => ({
+            _id: doc._id,
+            cropId: doc.cropId,
+            material: doc.material,
+            qtyUsed: doc.qtyUsed,
+            materialCost: doc.materialCost,
+          }));
+
+          // Create the response data
+          materialresponseData = materialCost;
+        })
+        .catch((err) => {
+          console.error(err);
+          res
+            .status(500)
+            .json({ msg: "Error saving material cost data to the database." });
+        });
+
+      ///////////////   Machinery Cost   /////////////////////
+      for (const machinerydetails of machineryCostDetails) {
+        const { method, isOwned, noUsed, days, machineryCost } =
+          machinerydetails;
+
+        if (!method || !isOwned || !noUsed || !days || !machineryCost) {
+          return res.status(400).json({ msg: "Please fill in all fields." });
+        }
+      }
+
+        // Create new machinery costs
+        await Machinery.insertMany(
+          machineryCostDetails.map((details) => ({
+            cropId,
+            ...details,
+          }))
+        )
+          .then((docs) => {
+            // docs contains the documents inserted with added _id fields
+            console.log("Machinery documents inserted to Collection");
+
+            // Create the machineryCost array
+            const machineryCost = docs.map((doc) => ({
+              _id: doc._id,
+              cropId: doc.cropId,
+              method: doc.method,
+              isOwned: doc.isOwned,
+              noUsed: doc.noUsed,
+              days: doc.days,
+              machineryCost: doc.machineryCost,
+            }));
+
+            // Create the response data
+            machineryresponseData = machineryCost;
+          })
+          .catch((err) => {
+            console.error(err);
+            res
+              .status(500)
+              .json({
+                msg: "Error saving machinery cost data to the database.",
+              });
+          });
+
+      const response = {
+        labourresponseData,
+        materialresponseData,
+        machineryresponseData,
+      };
+
+      // Send the response
+      res.status(200).json(response);
     } catch (err) {
       return res.status(500).json({ message: err.message });
     }
