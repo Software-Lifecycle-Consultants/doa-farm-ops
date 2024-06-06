7a49b5b3b30a27edb1bc29bb0a976c8eb9b48d65
iamarasekera <iamarasekera.dev@gmail.com>
2024-06-06 20:17:23 +0800

    Aded validation to check for empty values in username and pasword

--- schemas/login.schema.ts
+++ schemas/login.schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
 export const schemaLogin = z.object({
-  email: z.string().email('Please enter a valid email address'),
-  password: z.string().min(6, 'Please Enter Correct Password').max(100),
+  email: z.string().nonempty('Email is required').email('Please enter a valid email address'),
+  password: z.string().nonempty('Password is required').min(6, 'Please Enter Correct Password').max(100),
 });
